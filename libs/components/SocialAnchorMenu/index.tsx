b632c9a950f9abd03a7c696b922ea04a2129358b
irwanphan <irwanphan@gmail.com>
2022-09-10 16:54:41 +0700

    SocialAnchorMenu to bottom on scroll

--- libs/components/SocialAnchorMenu/index.tsx
+++ libs/components/SocialAnchorMenu/index.tsx
@@ -1,35 +1,58 @@
-import { Box, HStack } from "@chakra-ui/react"
+import { Box, Flex, HStack } from "@chakra-ui/react"
 import SocialAnchor from "@elements/SocialAnchor"
-import { FiMail, FiGithub, FiLinkedin, FiFeather, FiDribbble } from "react-icons/fi"
+import { FiMail, FiGithub, FiLinkedin, FiFeather, FiDribbble, FiInstagram } from "react-icons/fi"
 import { InView, useInView } from "react-intersection-observer"
 
 const SocialAnchorMenu = () => {
     const { ref, inView } = useInView({
-        threshold: 0
+        threshold: 0,
+        rootMargin: '8rem'
     })
     
     return (
         <InView>
             {({inView, ref}) =>
-                <Box py={8} w='max-content' mx='auto'>
-                    <HStack ref={ref} gap={2}>
-                        <SocialAnchor href='mailto:irwanphan@gmail.com' tooltip='email me'>
-                            <FiMail/>
-                        </SocialAnchor>
-                        <SocialAnchor href='https://github.com/irwanphan' tooltip='my repos'>
-                            <FiGithub/>
-                        </SocialAnchor>
-                        <SocialAnchor href='https://linkedin.com/in/irwanphan' tooltip='linked'>
-                            <FiLinkedin/>
-                        </SocialAnchor>
-                        <SocialAnchor href='https://behance.net/irwanphan' tooltip='behance'>
-                            <FiFeather/>
-                        </SocialAnchor>
-                        <SocialAnchor href='https://dribbble.com/irwanphan' tooltip='dribbble'>
-                            <FiDribbble/>
-                        </SocialAnchor>
-                    </HStack>
-                    <Box position='fixed' top={0} right={0}>{inView.toString()}</Box>
+                <Box>
+                    <Box ref={ref} h='6rem'
+                        position={inView ? 'absolute' : 'relative'}
+                    />
+                    <Flex  
+                        w='max-content' mx='auto'
+                        alignItems='center'
+                        position={!inView ? 'fixed' : 'relative'}
+                        bgColor={!inView ? 'whiteAlpha.900' : 'transparent'}
+                        h='6rem' 
+                        transition='0.3s ease all'
+                        zIndex={2}
+                        px={8}
+                        rounded='2xl'
+                        borderWidth='1px 2px 3px 1px'
+                        borderStyle='solid'
+                        borderColor={!inView ? 'gray.800' : 'transparent'}
+                        bottom={-4} left={0} right={0}        
+                        >
+                        <HStack gap={2}>
+                            <SocialAnchor href='mailto:irwanphan@gmail.com' tooltip='email me'>
+                                <FiMail/>
+                            </SocialAnchor>
+                            <SocialAnchor href='https://instagram.com/irwanphan' tooltip='my repos'>
+                                <FiInstagram/>
+                            </SocialAnchor>
+                            <SocialAnchor href='https://github.com/irwanphan' tooltip='my repos'>
+                                <FiGithub/>
+                            </SocialAnchor>
+                            <SocialAnchor href='https://linkedin.com/in/irwanphan' tooltip='linked'>
+                                <FiLinkedin/>
+                            </SocialAnchor>
+                            <SocialAnchor href='https://behance.net/irwanphan' tooltip='behance'>
+                                <FiFeather/>
+                            </SocialAnchor>
+                            <SocialAnchor href='https://dribbble.com/irwanphan' tooltip='dribbble'>
+                                <FiDribbble/>
+                            </SocialAnchor>
+                        </HStack>
+                        <Box position='fixed' top={0} right={0}>{inView.toString()}</Box>
+                    </Flex>
                 </Box>
             }
         </InView>
