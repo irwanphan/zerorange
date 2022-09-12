1001fb0bc8262442057c556711e165da48c35c5c
irwanphan <irwanphan@gmail.com>
2022-09-12 15:20:30 +0700

    fixed header

--- libs/components/BlockHeader/index.tsx
+++ libs/components/BlockHeader/index.tsx
@@ -1,10 +1,14 @@
-import { Box, HStack, Circle, Img, Heading, Divider, Text, Tooltip } from "@chakra-ui/react"
+import { Box, Circle, Img, Heading, Divider, Text, Tooltip, Flex } from "@chakra-ui/react"
 import PageSection from "@elements/Section"
 
 const BlockHeader = () => {
     return (
         <PageSection py={16}>
-            <HStack mx={'auto'} w='max-content'>
+            <Flex mx={'auto'} gap={2}
+                alignItems='center' justifyContent='center' 
+                textAlign={{ base: 'center', md: 'left' }}
+                wrap={{ base: 'wrap', md: 'nowrap' }}
+            >
                 <Circle size={40} overflow='hidden' mr={4}
                     borderStyle='solid'
                     borderColor='gray.800'
@@ -13,11 +17,11 @@ const BlockHeader = () => {
                 </Circle>
                 <Box>
                     <Heading><Tooltip label="こんにちわ">Hi</Tooltip>, I am Irwan</Heading>
-                    <Divider my={2}/>
+                    <Divider my={2} borderColor='gray.400'/>
                     <Text>I like to learn new things</Text>
                     <Text>I am a self starter. I am good at determining what to do to accomplish a defined goal</Text>
                 </Box>
-            </HStack>
+            </Flex>
         </PageSection>
     )
 }
