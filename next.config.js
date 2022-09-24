e49eaf6d7c92748ac7de2d6ca129e549efcdea2b
irwanphan <irwanphan@gmail.com>
2022-09-24 22:09:54 +0700

    changed pwa config rule

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withPWA = require('next-pwa')({
   dest: 'public'
 })
+const prod = process.env.NODE_ENV === 'production'
   
 module.exports = withPWA({
   // next.js config
   pwa: {
     disable: prod ? false : true,
     dest: 'public'
-  }
+  },
 })
