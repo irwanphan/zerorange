f112eef81178bb7ad6837872b31ee9b5cc91b3b2
irwanphan <irwanphan@gmail.com>
2022-09-30 17:25:17 +0700

    next.config.js for pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
-const settings = {
-  env: {
-  },
-  devIndicators: {
-    autoPrerender: false,
-  },
-  pwa: {
-    dest: 'public',
-  },
-};
-
-module.exports = process.env.NODE_ENV === 'development' ? settings : withPWA(settings);
+module.exports = withPWA({
+  // next.js config
+})
