d701a0edd31458e5cee395aa0e082b4d04aefa94
irwanphan <irwanphan@gmail.com>
2022-12-13 19:16:05 +0700

    lib update

--- libs/elements/AnchorMenu/index.tsx
+++ libs/elements/AnchorMenu/index.tsx
@@ -2,11 +2,31 @@ import { FlexProps, Link, Tooltip } from "@chakra-ui/react"
 
 interface AnchorMenuProps extends FlexProps {
     href: string
-    tooltip: string
+    tooltip?: string
 }
 
 const AnchorMenu
  = ({href, tooltip, children, ...rest}: AnchorMenuProps) => {
+    if (!tooltip) {
+        return (
+            <Link 
+                display='block' p={2}
+                fontSize={24}
+                transition='.4s ease all'
+                borderWidth='1px 2px 3px 1px'
+                borderStyle='solid'
+                borderColor='transparent'
+                _hover= {{ 
+                    borderColor: 'gray.800',
+                    shadow: 'lg',
+                    borderRadius: 'lg'
+                }}
+                href={href}>
+                {children}
+            </Link>
+        )
+    }
+    
     return (
         <Tooltip hasArrow label={tooltip} bgColor='gray.800' borderRadius='xl' px={3} py={2}>
             <Link 
