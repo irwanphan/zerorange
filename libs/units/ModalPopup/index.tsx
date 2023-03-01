ba531b71b4bf58ccda77942ebf32a146a7461108
irwanphan <irwanphan@gmail.com>
2023-03-01 14:52:28 +0700

    modal popup with same style

--- libs/units/ModalPopup/index.tsx
+++ libs/units/ModalPopup/index.tsx
@@ -1,4 +1,6 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, useDisclosure, FlexProps, Box } from "@chakra-ui/react"
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, useDisclosure, FlexProps, Box, Flex } from "@chakra-ui/react"
+import BubbleContainer from "@elements/BubbleContainer"
+import FormSubmitButton from "@elements/FormSubmit"
 import { MouseEventHandler, ReactNode } from "react"
 interface ModalProps extends FlexProps {
     modalProps: modalPropDetails
@@ -26,24 +28,30 @@ const ModalPopup = ({modalProps, children, isOpen, onClose, canCancel, ...rest}:
             motionPreset='slideInBottom'
         >
             <ModalOverlay />
-            <ModalContent p={3} w={'50rem'} {...rest}>
-                <ModalHeader
-                    textAlign={'center'} fontWeight={800}
-                >{modalProps.title}</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody textAlign={'center'}>
-                    <Box mb={2}>{modalProps.texts}</Box>
-                    { children && <Box>{children}</Box>}
-                </ModalBody>
-                <ModalFooter justifyContent={'center'}>
-                    { canCancel && 
-                        <Button colorScheme='blue' mr={2} variant={'outline'} 
-                        onClick={onClose}>{modalProps.cancel ?? 'Batal'}</Button>
-                    }
-                    <Button colorScheme='blue'
-                        onClick={modalProps.action}
-                    >{modalProps.button}</Button>
-                </ModalFooter>
+            <ModalContent 
+                // p={3} w={'50rem'} 
+                bg="transparent"
+                {...rest}>
+                <BubbleContainer>
+                    <ModalHeader
+                        textAlign={'center'} fontWeight={800}
+                    >{modalProps.title}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody textAlign={'center'}>
+                        <Box mb={2}>{modalProps.texts}</Box>
+                        { children && <Box>{children}</Box>}
+                    </ModalBody>
+                    <ModalFooter justifyContent={'center'} gap={2}>
+                            { canCancel && 
+                                <FormSubmitButton onClick={onClose} notLink>
+                                    {modalProps.cancel ?? 'Cancel'}
+                                </FormSubmitButton>
+                            }
+                            <FormSubmitButton onClick={modalProps.action} notLink bgColor='green.100'>
+                                {modalProps.button}
+                            </FormSubmitButton>
+                    </ModalFooter>
+                </BubbleContainer>
             </ModalContent>
         </Modal>
     )
